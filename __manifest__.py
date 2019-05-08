29c5bc480d293d7cc3ff9bccf544c6c7dbba5138
maryadi <maryadi071@gmail.com>
2019-05-08 08:57:22 +0700

    module belajar

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "seminar",
+
+    'summary': """
+        aplikasi seminar untuk belajar...""",
+
+    'description': """
+        belajar membuatmodul sederhana !
+    """,
+
+    'author': "maryadi cs",
+    'website': "http://www.m.com",
+
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/mainmenu.xml',
+        'views/mainpage.xml',
+    ],
+    
+    'application': True,   
+}
+
